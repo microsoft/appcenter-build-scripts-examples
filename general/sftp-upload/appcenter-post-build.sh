8695886da635dffb2d677d8e175a118b5966bd84
Daniel Adams <dan@bowstreet.de>
2018-02-21 22:10:35 +0100

    Fix sftp example

--- general/sftp-upload/appcenter-post-build.sh
+++ general/sftp-upload/appcenter-post-build.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 #
 # Upload the binary from the build output directory via SFTP using LFTP https://lftp.yar.ru/lftp-man.html
-# Make sure to use environment variables for the credentials
+# Make sure to create the environment variable $LFTP_PASSWORD in the App Center build configuration
 
 brew install lftp
 cd $APPCENTER_OUTPUT_DIRECTORY
