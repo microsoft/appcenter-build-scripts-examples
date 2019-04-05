0924e5adeeb6a6ed41704f68f7882f26e5cf9fc7
Daniel Adams <dan@bowstreet.de>
2019-04-05 15:36:20 +0200

    Update branch in flutter script

--- flutter/android-build/appcenter-post-clone.sh
+++ flutter/android-build/appcenter-post-clone.sh
@@ -9,7 +9,7 @@ set -e
 set -x
 
 cd ..
-git clone -b dev https://github.com/flutter/flutter.git
+git clone -b beta https://github.com/flutter/flutter.git
 export PATH=`pwd`/flutter/bin:$PATH
 
 flutter channel stable
